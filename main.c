ada59ce84fc231d0c93326809e618096fa157ec8
5000user5000 <37620231+5000user5000@users.noreply.github.com>
2022-03-03 15:16:53 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include <string.h>
 
-int getLength(char arr[256]);
+int getLength(char arr[257]);
 void copy(char a[],char m[]);
 void subtract(char m[],char n[]);
 void arrange(char m[],int len);
@@ -10,18 +10,11 @@ void multify(int times,char n[]);//先把要用的函數先宣告
 
 int main()
 {
-	char a[256]={},b[256]={},m[256]={},n[256]={};//一格只傳一個值 ,比int好用 ,最多只有256位數 ,先訂空位,不然會亂數 
+	char a[257]={0},b[257]={0},m[257]={0},n[257]={0};//一格只傳一個值 ,比int好用 ,最多只有256位數 ,先訂空位,不然會亂數 
 	int ans =1,mLength,nLength;
 	
 	scanf("%s",a);//輸入也變麻煩 ,看情況決定要不要用& ,還有注意最大位數在第一個a[0]處 
 	scanf("%s",b);
-	
-	/*int i=0;
-	while(a[i]) //output技術 
-	{
-		printf("%d",(int)a[i]-48);ascii code
-		i++;
-	}*/
 	
 	
 	//把大的給m,小的給n 
@@ -37,27 +30,33 @@ int main()
 	}
 	else//位數相同就只能老實比 
 	{
-		int i=0;
-		while(a[i])
+		int i=0,k=0;
+		while((int)a[i]>47)
 		{
 			if((int)a[i]>(int)b[i])
 			{ copy(a,m);
 		      copy(b,n);
-		      break;//跳出迴圈省時間 
+		      k=1;
+		      break;//即使跳出迴圈省時間 
 				
 			}
 			if((int)a[i]<(int)b[i])
 			{ copy(b,m);
 		      copy(a,n);
+		      k=1;
 		      break;
 				
 			}
+			if(i==getLength(a)-1&&k==0)//考慮兩數相同情況,用k是怕break後跑進來這if 
+			{
+			  copy(a,m);
+		      copy(b,n);
+		      break;
+			}
+			i++; 
 		}
 		
-	}
-	
-
-	  
+	}	  
 	
 	//開始找公因數 
 	while((int)m[0]-48>0 && (int)n[0]-48>0) 
@@ -82,10 +81,10 @@ int main()
 		nLength = getLength(n);
 		if(nLength>mLength) 
 		{
-			char temp[256]={};
+			char temp[257]={};
 			copy(m,temp);
 			copy(n,m);
-			memset(n, (char)(0), 256);//string.h,把n全歸零,不然copy的話,因為n位數多,無法用到n的尾巴 
+			memset(n, (char)(0), 257);//string.h,把n全歸零,不然copy的話,因為n位數多,無法用到n的尾巴 
 			copy(temp,n);
 		}
 		else if(nLength==mLength)
@@ -96,10 +95,10 @@ int main()
 				
 				if((int)n[i]>(int)m[i])
 				{
-					char temp[256]={};
+					char temp[257]={};
 			        copy(m,temp);
 			        copy(n,m);
-			        memset(n, (char)(0), 256);//歸0不是'0' 
+			        memset(n, (char)(0), 257);//歸0不是'0' 
 			        copy(temp,n);
 			        break;
 				
@@ -129,23 +128,26 @@ int main()
 
 /////////////////  功能函數      ////////////////////////
 
-int getLength(char arr[256])
+int getLength(char arr[257])
 {
 	int i=0;
 	while((int)arr[i]-48>=0)i++;//只要是0~9都會符合,如果是預設的0會變成-48 
 	return i;//注意不用+1,因為假設到 
 }
-void arrange(char m[],int len)//排列,把整個列往前挪
+void arrange(char m[],int len)//排列,把整個列往前挪,也許用成queue較好 
 {
 	
-	for(int i=0;i<len-1;i++ )m[i]=m[i+1];
-	if(len<256)m[len-1]=m[len];
-	else m[255] = (char)(0);//注意char 0和'0'是不同的,char 0的ascill code =0,扣-48 =-48 
+	for(int i=0;i<len-1;i++ )
+	    m[i]=m[i+1];
+	
+	m[len-1] = (char)(0);//不改變的話,尾數還會是48not0 
+//	if(len<256)m[len-1]=m[len];
+//	else m[255] = (char)(0);//注意char 0和'0'是不同的,char 0的ascill code =0,扣-48 =-48 ,直接256->257就不用這麼麻煩 
 	
 }
 void copy(char a[],char m[])//array本身就是指標,所以會改動 
 {	int i=0;
-	while(a[i]){
+	while((int)a[i]>47){
 	m[i]=a[i];
 	i++;
 	}
@@ -185,7 +187,7 @@ void subtract(char m[],char n[])//m = m-n
 		i--;
 		j--;
 	}
-	while((int)m[0] - 48==0)
+	while((int)m[0] - 48==0)//如果出現0026->026,所以必須用while 
 	{
 		arrange(m,mLen);
 		
@@ -196,36 +198,34 @@ void subtract(char m[],char n[])//m = m-n
 void half(char a[])//除以2
 {
 
-   int len = getLength(a);
+   int len = getLength(a),down=0,value;//down 退一位要補的數字 
    for(int i=0;i<len;i++)
    {
     
-	if( ((int)a[i] - 48)%2 !=0 && i!= len-1 )a[i+1] = (char)( (int)a[i+1] +10);
-	a[i] = (char)( ((int)a[i] - 48)/2+48);
-   	
-   	
+	value = (int)a[i] - 48;//因為a[i]會改動,這樣算down的值會變 
+	a[i] = (char)( (value)/2+48+down);
+	//if( (value)%2 !=0 && i!= len-1 ) 把a[i]挪在前面就不用看if 
+	down=(value*5)%10;// val*10/2後取尾數 
+	
    	
    }
    
-   while((int)a[0] - 48==0)
-   {
-		arrange(a,len);
+   if((int)a[0] - 48==0)
+    	arrange(a,len);
 		
-	}
+	
 }
 void multify(int times,char n[])
 {
 	int len = getLength(n),length=0;
 	int upNow=0,upPre=0;//進位 
-	if(len<10)
-	{
-	  for(int i=len;i>0;i--)
-	    n[i]=n[i-1];//先往後移,把首位空出來進位
-	  n[0] = '0';//首位補0 
-	  length =len;
-	  
-	} 
-	if(len==10) length=len-1;//多用length是因為如果只用len,len-1的話,arrarnge會錯亂 
+	 
+	for(int i=len;i>0;i--)
+	     n[i]=n[i-1];//先往後移,把首位空出來進位
+	n[0] = '0';//首位補0 
+	length =len;
+	
+	//if(len==10) length=len-1;//多用length是因為如果只用len,len-1的話,arrarnge會錯亂 
 	
 	for(int i=length;i>=0;i--)
     {
@@ -255,7 +255,7 @@ void multify(int times,char n[])
    
    while((int)n[0] - 48==0)//避免首位0 
    {
-		arrange(n,len+1);
+		arrange(n,length+1);
 		
 	}
 	
@@ -265,4 +265,3 @@ void multify(int times,char n[])
 }
  
  
-
